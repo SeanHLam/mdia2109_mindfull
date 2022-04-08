8d3746f94ae9a8334cd412b6425d6544050d1c5a
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-04-08 14:34:12 -0700

    getting error after comp

---
+++ comps/header.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const TextItem = styled.div`
+font-size: 48pt;
+`
+
+export function HeadText(){
+return <TextItem>text</TextItem>
+}
+
---
+++ pages/test.js
@@ -0,0 +1,9 @@
+import { HeadText } from "../comps/header";
+
+
+
+
+export default function Test(){
+    return <HeadText></HeadText>}
+
+
