fdad257cffe4872e5cd580f8b18ba6a92bc6dfcd
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-04-12 16:15:48 -0700

    navbar updates

--- comps/navbar.js
+++ comps/navbar.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faHouse, faLightbulb, faPhone, faFlag, faCircleInfo, faUserGroup, faX } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -8,12 +8,73 @@ const NavCont = styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
+flex-direction: column;
 
 background-color: #6F5F5E;
 `
 
-const LogoImage = styled.img`
+const NavTop = styled.div`
+width: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+padding: .5em;
+`
 
+const LogoImage = styled.img`
+ height:5em;
+ margin-right: .5em;
+ 
+`   
+const LogoType = styled.img`
+ height:3em;
+`
+
+const MenuCont = styled.div`
+ position: fixed;
+ left: 0;
+ margin-left: 1em;
+`
+
+const NavBot = styled.div`
+display: flex;
+width: 100%;    
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const NavUl = styled.ul`
+ display: flex;
+ flex-direction: column;
+ padding:0;
+ width:100%;
+ margin: 0;
+
+
+`
+
+const NavLi = styled.li`
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+width: 100%;
+font-family: Oswald;
+color: white;
+font-size: 32pt;
+list-style-type: none;
+transition: background-color 1s;
+&:hover{
+    background-color: #DAE5E7;
+    color: black;
+}
+
+
+`
+
+const IconCont = styled.div`
+ margin-right .3em;
 `
 
 
@@ -22,11 +83,34 @@ export function NavBar()
 {
 return <NavCont>
     
-    
-    test
-    <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
-    <LogoImage src="browny2.svg"></LogoImage>
+    <NavTop
+    >   <MenuCont>
+            <FontAwesomeIcon icon={faBars} size="3x" color="white"></FontAwesomeIcon>
+        </MenuCont> 
+        <LogoImage src="browny1.svg"></LogoImage>
+        <LogoType src="LogoType.svg"></LogoType>
+    </NavTop>
 
-    </NavCont>
+    <NavBot>
+        <NavUl>
+
+            <NavLi>
+                <IconCont>
+                    <FontAwesomeIcon icon={faHouse}  color="white"></FontAwesomeIcon>
+                </IconCont>
+                Home
+            </NavLi>
+            <NavLi>
+                <IconCont>
+                    <FontAwesomeIcon icon={faHouse}  color="white"></FontAwesomeIcon>
+                </IconCont>
+                Home
+            </NavLi>
+
+
+        </NavUl>
+
+    </NavBot>
+</NavCont>
 }
 
