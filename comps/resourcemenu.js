5ce8a64212ebe7bd91f8ee9df69ee8c67f89ef9b
Jason Kwak <jasonkwak@Jasons-MacBook-Pro.local>
2022-04-17 22:35:11 -0700

    updated resource menu

--- comps/resourcemenu.js
+++ comps/resourcemenu.js
@@ -1,27 +1,36 @@
 import styled from "styled-components";
 import {numberArr, linkArr} from "../data/resourcedata";
 import {useState} from 'react';
+import { faPhone, faLink, faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Resource from "../pages/resource";
 
 const ItemList = styled.li`
 font-size: 14pt;
-color:black;
-text-align:left;
+font-family: 'Oswald', sans-serif;
+color:#6F5F5E;
 list-style: none;
+display:flex;
+align-items:center;
 `
 const ResourceOutline = styled.div`
-width: 200px;
-height: 350px;
+width: 220pt;
+height: 200pt;
 background-color:#DAE5E7;
 border: 1px solid black;
-border-radius: 25px;
-`
-const ToggleButton = styled.button`
-width: 10000pt;
+border-radius: 26px;
+box-sizing:content-box !important;
+overflow-y:auto;
+overflow-x:hidden;
+white-space:nowrap;
 `
 
-export function ResourceBox(){
-    return <ResourceOutline></ResourceOutline>
-}
+const ResourceBox = styled.div`
+display:flex;
+justify-content:center;
+padding:15px;
+`
+
 
 export function Item({
     text = "Item"
@@ -43,20 +52,64 @@ export default function List({
     </div>
 }
 
+
+
 export function ResourceMenu(){
     const [fav, setFav] = useState("number");
-    const ChangeList = () => {
+    const [background, setBackground] = useState("#D28A7C");
+    
+    /*const setStyle = (background, icon) => {
+        setBackground(background);
+        setIcon(icon);
+    };*/
+    var backGround = "";
+    if (background === "#D28A7C"){
+        backGround = "#8E6057";
+    } else if (background === "#8E6057"){
+        backGround = "#D28A7C";
     }
+
+    const ToggleButton = styled.button`
+    color:white;
+    text-align:center;
+    font-family:'Oswald', sans-serif;
+    border-color: black;
+    font-size:16pt;
+    width: 110pt;
+    border-radius: 25px 0px 0px 0px;
+    border:none;
+    background-color:${background};
+    padding: 5pt 15pt 5pt 15pt;
+    `
+
+    const rightbutton = {
+    borderRadius:"0px 25px 0px 0px"
+    }
+
     var listArr = [];
+
     if (fav === "number"){
-        listArr = numberArr;
+        listArr = numberArr; 
+
     } else if (fav === "link"){
         listArr = linkArr;
     }   
+
     return <div>
-        <ToggleButton onClick={()=>setFav("number")}> Numbers </ToggleButton>
-        <button onClick={()=>setFav("link")}> Links </button>
-        <List arr= {listArr}/>
+    <ResourceBox>
+            <ResourceOutline>
+
+                <ToggleButton 
+                    onClick={ () => { setFav("number"); setBackground("#8E6057");} }>
+
+                    <FontAwesomeIcon icon={faPhone} size="1x" color="#8E6057" marginRight="5px"> </FontAwesomeIcon>  NUMBERS </ToggleButton>
+                
+                <ToggleButton
+                onClick={()=>setFav("link")} style= {rightbutton}>
+                    <FontAwesomeIcon icon={faLink} size="1x" color="#8E6057" marginRight="5px"> </FontAwesomeIcon>  LINKS </ToggleButton>
+                <List arr= {listArr}/>
+            </ResourceOutline>
+        </ResourceBox>
     </div>
 
 }
--- data/resourcedata.js
+++ data/resourcedata.js
@@ -1,13 +1,31 @@
 export const numberArr = [
-    "BC Mental Health Support: 310-6789",
-    "Crisis Services Canada: 1 (833)-456-4566",
-    "Fraser Health Crisis Line: 1 (877)-820-7444",
-    "Here2Talk: 1 (877)-857-3397",
-    "Emergencies: 911",
-    "Healthlink: 811",
-]
+    "BC Mental Health Support:",
+    "310-6789",
+    "Crisis Services Canada:",
+    "1 (833)-456-4566",
+    "Fraser Health Crisis Line:",
+    "1 (877)-820-7444",
+    "Here2Talk:",
+    "1 (877)-857-3397",
+    "Emergencies:",
+    "911",
+    "Healthlink:",
+    "811"
+];
+
 
 export const linkArr = [
-    "Chat Service for Youth: www.YouthInBC.com",
-    "Chat Service for Adults: www.CrisisCentreChat.com"
+    "Chat Service for Youth:",
+    "www.YouthInBC.com",
+    "Chat Service for Adults:",
+    "www.CrisisCentreChat.com",
+    "Canada Mental Health:",
+    "https://cmha.ca/",
+    "BetterHelp Private Therapy:",
+    "www.BetterHelp.com/helpme",
+    "Here2Talk for Students:",
+    "www.Here2Talk.ca",
+    "TogetherAll:",
+    "Government of Canada:",
+    "www.canada.ca/public-health/topics/mental-health-wellness"
 ]
