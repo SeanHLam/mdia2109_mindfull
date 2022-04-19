84c57c53b25f8a5e9f2fac5931a32399f19fd46b
mmogk <mmogk@my.bcit>
2022-04-19 13:18:16 -0700

    some img pages

--- comps/images.js
+++ comps/images.js
@@ -6,10 +6,12 @@ const ImageItem = styled.img`
 width: 30%;
 height: auto;
 padding: 20pt;
+margin-left: auto;
+margin-right: auto;
 position: relative;
 display: block;
 `
 
-export function ImgDiv({path="/browny2.svg"}){
-return <ImageItem src={path}/> 
+export function ImgDiv({path="/browny2.svg", size="20%", placement="relative", opacity="100%"}){
+return <ImageItem src={path} style={{width: size, position: placement, opacity: opacity}}/> 
 }
